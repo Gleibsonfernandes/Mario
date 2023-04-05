ba085c9aa542072d4845bebe2393fb90fad0167b
Gleibsonfernandes <gleibsonfernandes17@gmail.com>
2023-04-05 16:28:51 -0300

    Conclusão

--- img/script.js
+++ img/script.js
@@ -1,8 +1,11 @@
 let mario = document.querySelector('.mario');
+let mario_img = document.querySelector('.mario_img');
 let cano = document.querySelector('.cano');
 let nuvem = document.querySelector('.nuvem');
 let game_over = document.querySelector('.gameOver')
-
+let cont_pontos = document.querySelector('.cont')
+let progresso = document.querySelector('.progresso')
+let cont = 0;
 const jump = () =>{
     mario.classList.add('pulo')
 
@@ -15,37 +18,49 @@ const loop = setInterval(() =>{
     let position_cano = cano.offsetLeft;
     let position_nuvem =nuvem.offsetLeft;
     let mario_position = +window.getComputedStyle(mario).bottom.replace('px', '');
+    let pontos = document.querySelector('.pontos')
     
-    
+    //console.log("posicao do cano:" + position_cano);
 
-    let cont =0;
     
-    while(Number(position_cano) <-30){
-        cont = cont + 10;
-        console.log(cont)
-    }
-    
+     
     if(position_cano <= 60 && mario_position <52 && position_cano >0 ){
         cano.style.animation ="none";
         cano.style.left = `${position_cano}px`
-        nuvem.style.animation ="none";
+        nuvem.style.animation ="none"; 
         nuvem.style.left = `${position_nuvem}px`
         game_over.style.display='block'
 
         mario.style.animation ="none";
         mario.style.bottom = `${mario_position}px`;
-        mario.img ='mario_gameOver.png'
+        mario_img.setAttribute('src', '../img/mario_gameOver.png')
+        mario_img.style.width ='50px';
+        progresso.innerText=`${ ''}`;
+    }
+    // console.log(position_cano);
+    if(Number(position_cano) <-55 ){
+        let pontos = cont+=1030;
         
+        cont_pontos.innerText=`${pontos}`
+
+            if(pontos > 20000){
+                 progresso.innerText=`${ 'Verry nyce!'}`;
+
+                 setTimeout(() =>{
+                    progresso.innerText=`${ ''}`;
+                }, 3000); 
+             }
     }
 
     
-    
 }, 10)
 
+    
 
+   
+   
 
 document.addEventListener('keydown', jump);
-
 
 
 
