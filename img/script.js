012dd404bf30a51df135e66bed5fede6b2f10c28
Gleibsonfernandes <gleibsonfernandes17@gmail.com>
2023-04-04 22:22:33 -0300

    testando logica de pontos

--- img/script.js
+++ img/script.js
@@ -1,4 +1,7 @@
 let mario = document.querySelector('.mario');
+let cano = document.querySelector('.cano');
+let nuvem = document.querySelector('.nuvem');
+let game_over = document.querySelector('.gameOver')
 
 const jump = () =>{
     mario.classList.add('pulo')
@@ -8,5 +11,43 @@ const jump = () =>{
     }, 500); 
 }
 
+const loop = setInterval(() =>{
+    let position_cano = cano.offsetLeft;
+    let position_nuvem =nuvem.offsetLeft;
+    let mario_position = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    
+
+    let cont =0;
+    
+    while(Number(position_cano) <-30){
+        cont = cont + 10;
+        console.log(cont)
+    }
+    
+    if(position_cano <= 60 && mario_position <52 && position_cano >0 ){
+        cano.style.animation ="none";
+        cano.style.left = `${position_cano}px`
+        nuvem.style.animation ="none";
+        nuvem.style.left = `${position_nuvem}px`
+        game_over.style.display='block'
+
+        mario.style.animation ="none";
+        mario.style.bottom = `${mario_position}px`;
+        mario.img ='mario_gameOver.png'
+        
+    }
+
+    
+    
+}, 10)
+
+
+
 document.addEventListener('keydown', jump);
+
+
+
+
+
 
